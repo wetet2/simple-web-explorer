1c5190b1b938f949fbbdd8df2ba04d948d4a9100
HoyoonLee <wetet2@gmail.com>
2018-04-27 16:35:38 +0900

    modified

--- routes/index.js
+++ routes/index.js
@@ -29,7 +29,7 @@ router.get('/*/', function(req, res, next) {
                         folderArr: [],
                         fileArr : [],
                         path: url,
-                        prevPath: ''
+                        prevPath: '',
                     });
                     return;
                 }
